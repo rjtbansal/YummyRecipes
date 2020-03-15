82213bb54c02e187403e47a2679dca5f03331a53
Rajat Bansal <rjtbansal@gmail.com>
2020-03-15 14:26:30 -0400

    endpoint to get recipes by cuisine id

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const recipesRoutes = require('./routes/recipes');
 const categoryRoutes = require('./routes/category');
+const cuisineRoutes = require('./routes/cuisine');
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(cors());
 
 app.use('/recipes',recipesRoutes);
 app.use('/category', categoryRoutes);
+app.use('/cuisine', cuisineRoutes);
+
 
 //connecting to our cloud MongoDb
 const mongoCloudUrl = 'mongodb+srv://admin:adminadmin@yummyrecipescluster-0g9s7.mongodb.net/YummyRecipes?retryWrites=true&w=majority';
---
+++ server/routes/cuisine.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Cuisine = require('../models/cuisine');
+const Recipe = require('../models/recipe');
+
+/**
+ Endpoint: /cuisine/:id
+ Outcome: List of recipes belonging to a particular cuisine
+ */
+
+ router.get('/:id', (req, res) => {
+    const recipesReceived = [];
+    Cuisine.findById(req.params.id, 'mealIds', (err, cuisine) => {
+        if(!err) {
+            if(cuisine) {               
+                cuisine.mealIds.forEach(mealId => {
+                    Recipe.findById(mealId, 'name category cuisine image' , (err, recipe) => {
+                        recipesReceived.push(recipe);
+                    });
+                });
+                setTimeout(() => res.json(recipesReceived), 200);
+            }
+        }
+        else {
+            console.error(err);
+        }
+  });
+ });
+ module.exports = router;
