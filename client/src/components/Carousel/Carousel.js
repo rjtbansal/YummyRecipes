cdac5730dea53bc9566fde28279f6a3eda5ab839
Rajat Bansal <rjtbansal@gmail.com>
2020-03-16 17:12:07 -0400

    added carousel with dummy data, need to improve its styling

---
+++ client/src/components/Carousel/Carousel.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './Carousel.scss';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
+
+export default class Carousel extends Component {
+    render() {
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        className: 'mycarousel'
+      };
+      return (
+        <div>
+          <Slider {...settings}>
+              {this.props.children}
+          </Slider>
+        </div>
+      );
+    }
+  }
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,59 @@
 import React from 'react';
-
+import Carousel from '../../components/Carousel/Carousel';
+import axios from 'axios';
 const Home = () => {
-    return <h2>
-        Home Page
-    </h2>
+
+    
+
+    return <div>
+        <h3>Today's Recipes</h3>
+        {/* now call api and render data same as below */}
+        <Carousel>
+            <div>
+                 <h3>Recipe1</h3>
+            </div>
+            <div>
+                 <h3>Recipe2</h3>
+            </div>
+            <div>
+                 <h3>Recipe3</h3>
+            </div>
+            <div>
+                 <h3>Recipe4</h3>
+            </div>
+            <div>
+                 <h3>Recipe5</h3>
+            </div>
+            <div>
+                 <h3>Recipe6</h3>
+            </div>
+        </Carousel>
+
+        <h3>Explore Our Cuisines</h3>
+        <Carousel>
+            <div>
+                 <h3>Cuisine1</h3>
+            </div>
+            <div>
+                 <h3>Cuisine2</h3>
+            </div>
+            <div>
+                 <h3>Cuisine3</h3>
+            </div>
+            <div>
+                 <h3>Cuisine4</h3>
+            </div>
+            <div>
+                 <h3>Cuisine5</h3>
+            </div>
+            <div>
+                 <h3>Cuisine6</h3>
+            </div>
+        </Carousel>
+
+        <h3>Explore Our Categories</h3>
+        <Carousel />
+    </div>
 }
 
 export default Home;
