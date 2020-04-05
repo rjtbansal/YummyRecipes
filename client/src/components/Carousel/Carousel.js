6b48d9441a8a6288b1d869488ed51c924b9345fa
Rajat Bansal <rjtbansal@gmail.com>
2020-04-05 12:00:53 -0400

    made carousal responsive

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -11,8 +11,32 @@ export default class Carousel extends Component {
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 3,
-        className: 'mycarousel'
+        slidesToScroll: 4,
+        className: 'mycarousel',
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 430,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false
+            }
+          }
+        ]
       };
       return (
         <div>
